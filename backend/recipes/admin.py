5e05695d26cc9d9b0a3e84d5df11e01157ebd8c9
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-09-22 00:30:55 +0500

    Basic recipes models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,60 @@
 from django.contrib import admin
 
-from .models import (Tags)
+from .models import (FavouriteRecipes,
+                     Ingredients,
+                     IngredientsInRecipe,
+                     Recipes,
+                     Tags
+                     )
+
+
+"""
+Как должна быть настроена админка
+В интерфейс админ-зоны нужно вывести необходимые поля моделей и настроить фильтры:
+
+    вывести все модели с возможностью редактирования и удаление записей;
+    для модели пользователей добавить фильтр списка по email и имени пользователя;
+    для модели рецептов:
+      
+        в списке рецептов вывести название и имя автора рецепта;
+        добавить фильтры по автору, названию рецепта, тегам;
+        на странице рецепта вывести общее число добавлений этого рецепта в избранное;
+    для модели ингредиентов:
+      
+        в список вывести название ингредиента и единицы измерения;
+        добавить фильтр по названию.
+"""
 
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
     list_display = ('pk', 'name', 'slug', 'color')
     list_editable = ('name', 'slug', 'color')
+
+
+@admin.register(Recipes)
+class RecipesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'author')
+    list_editable = ('name',)
+    list_filter = ('name', 'author', 'tags')
+    search_fields = ('name', 'author')
+
+
+@admin.register(Ingredients)
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(IngredientsInRecipe)
+class IngredientsInRecipeAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'recipe', 'ingridient', 'amount')
+    list_editable = ('recipe', 'ingridient', 'amount')
+
+
+@admin.register(FavouriteRecipes)
+class FavouriteRecipesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user', 'recipe')
+    list_editable = ('user', 'recipe')
+    search_fields = ('name', 'recipe')
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -43,6 +43,36 @@ class Tags(models.Model):
 
 
 """
+Ингредиент
+Данные об ингредиентах должны храниться в нескольких связанных таблицах.
+На стороне пользователя ингредиент должен содержать следующие атрибуты:
+
+    Название.
+    Количество.
+    Единицы измерения.
+
+Все поля обязательны для заполнения.
+"""
+
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        verbose_name='Название ингредиента', max_length=100
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единица измерения', max_length=16
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}.'
+
+
+"""
 Рецепт
 Атрибуты модели:
 
@@ -75,7 +105,11 @@ class Recipes(models.Model):
     text = models.TextField(
         verbose_name='Описание рецепта',
     )
-    # ingredients
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        through='IngredientsInRecipe',
+        verbose_name='Список ингредиентов',
+    )
     tags = models.ManyToManyField(
         Tags,
         verbose_name='Список тегов',
@@ -98,33 +132,51 @@ class Recipes(models.Model):
         return f'{self.name}'
 
 
-"""
-Ингредиент
-Данные об ингредиентах должны храниться в нескольких связанных таблицах. На стороне пользователя ингредиент должен содержать следующие атрибуты:
-
-    Название.
-    Количество.
-    Единицы измерения.
-
-Все поля обязательны для заполнения.
-"""
-
-
-class Ingredients(models.Model):
-    name = models.CharField(
-        verbose_name='Название ингредиента', max_length=100
+class IngredientsInRecipe(models.Model):
+    recipe = models.ForeignKey(
+        Recipes,
+        on_delete=models.CASCADE,
+        related_name='ingredients_amount',
+        verbose_name='Рецепт',
     )
-    counts = models.PositiveSmallIntegerField(
-        verbose_name="Количество", default=None
+    ingredient = models.ForeignKey(
+        Ingredients,
+        on_delete=models.CASCADE,
+        related_name='ingredients_amount',
+        verbose_name='Ингредиент',
     )
-    measurement_unit = models.CharField(
-        verbose_name='Единица измерения', max_length=16
+    amount = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество в рецепте',
     )
 
     class Meta:
-        verbose_name = 'Ингредиент'
-        verbose_name_plural = 'Ингредиенты'
-        ordering = ('name',)
+        verbose_name = 'Ингредиент для рецепта'
+        verbose_name_plural = 'Ингредиенды для рецепта'
+        ordering = ('id',)
 
     def __str__(self):
-        return f'{self.name}, {self.measurement_unit}.'
+        return f'{self.ingredient.name} - {self.amount}'
+
+
+class FavouriteRecipes(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favourites',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipes,
+        on_delete=models.CASCADE,
+        related_name='favourites',
+        verbose_name='Рецепт',
+    )
+
+    class Meta:
+        verbose_name = 'Любимый рецепт'
+        verbose_name_plural = 'Любимые рецепты'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return f'{self.user} - {self.recipe.name}'
