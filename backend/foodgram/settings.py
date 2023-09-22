3bd27d0d6c14dca5d8b2fde6a2a54e5918faeb95
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-09-23 02:18:17 +0500

    Add basic throttling

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,6 +122,14 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PAGINATION_CLASS": "api.paginations.StandardResultsSetPagination",
     "PAGE_SIZE": 6,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.UserRateThrottle',
+        'rest_framework.throttling.AnonRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'user': '1000/day',
+        'anon': '100/day',
+    }
 }
 
 DJOSER = {
