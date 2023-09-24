eeec6d34398520fdf5899e0680542ece896145da
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-09-24 12:09:29 +0500

    users model rewrite

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,21 +1,106 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-User = get_user_model()
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+        self,
+        email,
+        username,
+        first_name,
+        last_name,
+        password,
+    ):
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(
+        self,
+        email,
+        username,
+        first_name,
+        last_name,
+        password,
+    ):
+        user = self.create_user(
+            email,
+            password=password,
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+        )
+        user.is_active = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
-class Follow(models.Model):
+class CustomUser(AbstractUser):
+    email = models.EmailField(
+        max_length=254,
+        unique=True,
+        verbose_name='email',
+    )
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name='Имя пользователя',
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия',
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
+
+    def get_full_name(self):
+        return f'{self.first_name}  {self.last_name}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Subscriber(models.Model):
     user = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='follower',
+        CustomUser,
         verbose_name='Подписчик',
+        related_name='subscriber',
+        on_delete=models.CASCADE,
     )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='following',
+        CustomUser,
         verbose_name='Автор',
+        related_name='author',
+        on_delete=models.CASCADE,
     )
 
     class Meta:
@@ -24,13 +109,13 @@ class Follow(models.Model):
         ordering = ('id',)
         constraints = [
             models.UniqueConstraint(
-                fields=(
+                fields=[
                     'user',
                     'author',
-                ),
-                name='unique_follow',
+                ],
+                name='unique_subscription'
             ),
         ]
 
-    def __str__(self):
-        return f'{self.user} подписан на {self.author}'
+    def __str__(self) -> str:
+        return f'{self.user}подписан на {self.author}'
