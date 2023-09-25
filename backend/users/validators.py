e1de730fffeda3b88f319fd27fb2eb74a742caa8
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-09-25 22:13:43 +0500

    quick fix

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -3,8 +3,4 @@ from django.core.validators import RegexValidator
 
 def validate_username(username):
     if username.lower() == 'me':
-        raise ValidationError(f'Имя "{str(username)}" запрещено.')
-    regex_validator = RegexValidator(
-        regex='^[a-zA-Z0-9/.+-]+$',
-        message='Разрешены буквы, цифры и символы ., @, +, - ',
-    )
+        raise ValidationError(f'Использовать имя "{str(username)}" запрещено.')
