6d5fb103f5440d44c08c3c648d8424da44707960
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-10-02 12:35:33 +0500

    User model small typo fix

--- backend/users/models.py
+++ backend/users/models.py
@@ -53,8 +53,8 @@ class Subscribe(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Подписка',
-        verbose_name_plural = 'Подписки',
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
         constraints = [
             models.UniqueConstraint(
                 fields=[
