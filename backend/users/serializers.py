db09a1828e8bbaff07eb3c7dc5afc1896900e635
Алексей Зюркалов <aleksey.zyurkalov@gmail.com>
2023-10-10 01:12:53 +0500

    Standart ModelSerializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer, UserSerializer
+from djoser.serializers import UserCreateSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
 
