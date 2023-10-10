6bd608f16407887fb5696ada8a16039d7f7e3ba6
Aleksey Zyurkalov <aleksey.zyurkalov@gmail.com>
2023-10-10 22:51:54 +0500

    new typo fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -21,7 +21,7 @@ from recipes.models import (
     Tags, User,
 )
 from users.models import CustomUser, Subscribe
-from .filters import RecipeFilter, IngredientsFilter
+from .filters import RecipeFilter, IngredientFilter
 from .pagination import CustomPagination
 from .serializers import (
     FavouriteSerializer,
@@ -44,7 +44,7 @@ class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredients.objects.all()
     serializer_class = IngredientSerializer
     search_fields = ('^name',)
-    filter_backends = (IngredientsFilter,)
+    filter_backends = (IngredientFilter,)
     pagination_class = None
     permission_classes = [IsAuthenticatedOrReadOnly, ]
 
